181dc9b1fb0b28db192ad7ec9c08d7b796a75a88
Boris <borismilkman@gmail.com>
2016-02-09 23:07:49 +0300

    Adding screens :D

--- android/src/com/turnbasedgame/game/AndroidLauncher.java
+++ android/src/com/turnbasedgame/game/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.turnbasedgame.game.TurnBasedGane;
+import com.turnbasedgame.game.TurnBasedGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new TurnBasedGane(), config);
+		initialize(new TurnBasedGame(), config);
 	}
 }
