9ae3d0ec4ffe30ea1f27ebad53e466a43e72bebe
Boris <borismilkman@gmail.com>
2016-02-09 10:15:44 +0300

    Initial commit :D

---
+++ ios/src/com/turnbasedgame/game/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.turnbasedgame.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.turnbasedgame.game.TurnBasedGane;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new TurnBasedGane(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
